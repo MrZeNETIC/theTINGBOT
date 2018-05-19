05c4d8ebb51e925db0a1badcd00b616ee045c8df
MrZeNETIC <39440623+MrZeNETIC@users.noreply.github.com>
2018-05-19 15:06:41 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'please') {
+    	message.reply('no');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
