69d087cf8c89e0d223e67edca06a3132220d18cb
MrZeNETIC <39440623+MrZeNETIC@users.noreply.github.com>
2018-05-19 15:38:30 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,12 +32,12 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!joke') {
-    	 randomjoke = random[joke1, joke2]
-        
+    	randomjoke = random[joke1, joke2]
+        message.reply randomjoke
   	}
 });
-joke1 = message.reply('Whats the best time to go to the dentist, tooth-hurty')
-joke1 = message.reply('Im reading a book about anti-gravity. Its imposibble to put down"
+joke1 = 'Whats the best time to go to the dentist, tooth-hurty'
+joke1 = 'Im reading a book about anti-gravity. Its imposibble to put down'
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
