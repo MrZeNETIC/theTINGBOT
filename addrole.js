2d215d0c685b7e6aff22a52ad28adc041c57158c
MrZeNETIC <39440623+MrZeNETIC@users.noreply.github.com>
2018-05-25 08:38:05 -0600

    Update addrole.js

--- addrole.js
+++ addrole.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 module.exports.run = async (bot, message, args) => {
 
