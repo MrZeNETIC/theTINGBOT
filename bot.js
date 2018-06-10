38b523b00a677235a3e2e324d95d88dd05fdd71e
MrZeNETIC <39440623+MrZeNETIC@users.noreply.github.com>
2018-06-10 16:43:43 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,11 @@ client.on('message', message => {
   	}
 });
 
-
+client.on('message', message => {
+    if (message.content === 'who is ting') {
+    	message.channle.send ('Well my good sir, you see ting is the best of us all \nHe is the lord, at everything, trully brings out the best in all of us);
+  	}
+});
 
 client.on('message', message => {
     if (message.content === '.help') {
