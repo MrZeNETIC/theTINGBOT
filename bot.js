914f2754156e963a75d9a57e4a433cecd6ec89f4
MrZeNETIC <39440623+MrZeNETIC@users.noreply.github.com>
2018-06-10 15:46:59 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var memes = [
+    "https://i.pinimg.com/originals/eb/8a/38/eb8a385b4555ba3e66463d02cc2b5a94.jpg",
+    "https://pics.me.me/priest-do-you-take-my-man-as-your-hubby-girl-5369999.png",
+    "https://pics.me.me/when-you-buy-rice-krispies-mans-not-hot-michael-bapaah-27576125.png",
+    "https://pics.me.me/when-youre-showing-someone-a-video-and-they-tap-the-20604920.png",
+    "https://pics.me.me/when-you-holla-at-a-peng-ting-and-her-friends-10591348.png"
+];
+
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
+
+if (message.content === '~help') {
+    var meme = Math.random() * (2 - 0) + 0;
+    message.channel.send("Here's you a meme!",{files[memes[meme]]});
 
 client.on('message', message => {
     if (message.content === 'ping') {
